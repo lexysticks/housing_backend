abd7035f55716557d6f5529b08bf70ecbc951e76
lexysticks <olemmanuel@1528>
2025-11-01 08:04:33 +0100

    Fix Redis connection issue for Render deployment

--- service/settings.py
+++ service/settings.py
@@ -113,13 +113,19 @@ DATABASES = {
     )
 }
 
+# CACHES = {
+#     "default": {
+#         "BACKEND": "django_redis.cache.RedisCache",
+#         "LOCATION": "redis://127.0.0.1:6379/1",  # Redis host, port, db
+#         "OPTIONS": {
+#             "CLIENT_CLASS": "django_redis.client.DefaultClient",
+#         }
+#     }
+# }
 CACHES = {
     "default": {
-        "BACKEND": "django_redis.cache.RedisCache",
-        "LOCATION": "redis://127.0.0.1:6379/1",  # Redis host, port, db
-        "OPTIONS": {
-            "CLIENT_CLASS": "django_redis.client.DefaultClient",
-        }
+        "BACKEND": "django.core.cache.backends.locmem.LocMemCache",
+        "LOCATION": "unique-snowflake",
     }
 }
 
